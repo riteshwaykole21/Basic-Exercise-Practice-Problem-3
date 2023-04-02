d68d0d31b341da0e7a52c53caa62bab2bdf83253
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-02 15:09:53 +0530

    [Ritesh]add : To Check The Value And Sum is 20

---
+++ BasicExercise/CheckSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    public class CheckSum
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Enter The First Number");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Second Number ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+            if (X == 20)
+            {
+                Console.WriteLine("True");
+            }
+            else if (Y == 20)
+            {
+                Console.WriteLine("True");
+            }
+            else if (X+Y == 20)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
---
+++ BasicExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Check The Sum Of Two Given Interger");
+            Console.WriteLine("Enter The Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    CheckSum checkSum = new CheckSum();
+                    checkSum.Solution();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
