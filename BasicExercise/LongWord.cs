567a448f12986741ae0e30e75cf6dc55e8f147e8
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-02 21:44:25 +0530

    [Ritesh]add : 1 To 100 Odd Number

---
+++ BasicExercise/LongWord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    public class LongWord
+    {
+        public void DataTest()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.WriteLine( i );
+                }
+            }
+        }
+    }
+}
--- BasicExercise/Program.cs
+++ BasicExercise/Program.cs
@@ -12,6 +12,7 @@ namespace BasicExercise
         {
             Console.WriteLine("1 : Check The Sum Of Two Given Interger");
             Console.WriteLine("2 : Convert String Into Lower Case");
+            Console.WriteLine("3 : All Odd Number From 1 to 99");
             Console.WriteLine("Enter The Option");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch (Option)
@@ -23,6 +24,10 @@ namespace BasicExercise
                 case 2:
                     GivenString givenString = new GivenString();
                     givenString.Sample();
+                    break;
+                case 3:
+                    LongWord longWord = new LongWord();
+                    longWord.DataTest();
                     break;
             }
             Console.ReadLine();
